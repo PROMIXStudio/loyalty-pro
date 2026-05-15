640220170df363663ac72fec850a00bb5a79c351
PROMIXStudio <promixstudio24@gmail.com>
2026-05-15 12:06:58 +0100

    Replace vite with static assets

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,26 +1,17 @@
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
-
     <meta charset="UTF-8">
-
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Loyalty Pro</title>
 
-    @viteReactRefresh
-
     <link rel="stylesheet" href="/build/assets/app-BWoHBg36.css">
-<script type="module" src="/build/assets/app-BSQo_1mS.js"></script>
-
 </head>
-
 <body>
 
     <div id="app"></div>
 
-</body>
+    <script type="module" src="/build/assets/app-BSQo_1mS.js"></script>
 
+</body>
 </html>
