dfb4a3352f95444cdd314c2aac100003b39a39f7
PROMIXStudio <promixstudio24@gmail.com>
2026-05-15 11:23:43 +0100

    Rebuild Vite assets with HTTPS

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://loyalty-pro-production.up.railway.app'),
 
     /*
     |--------------------------------------------------------------------------
